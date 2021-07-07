78fa07e5b60e3ea5f437a14d63dfe3eb642a305f
OEGBVerlag <serviceteam@oegbverlag.at>
2021-07-07 13:44:37 +0200

    EX-1477: add boilerplate files

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,6 @@
-/*
-Copyright 2020 Adobe
-All Rights Reserved.
-
-NOTICE: Adobe permits you to use, modify, and distribute this file in
-accordance with the terms of the Adobe license agreement accompanying
-it.
-*/
-
 // Stylesheets
-import "./main.scss";
+import './main.scss'
 
 // Javascript or Typescript
-import "./**/*.js";
-import "./**/*.ts";
+import './shims/*.js.js.js'
+import './components/**/*.ts'
